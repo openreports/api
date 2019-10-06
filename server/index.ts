d6af35736fd757c7f2213dc7a7ceef2ceae920af
Dhruv Jain <dhruvjainpenny@gmail.com>
2019-10-06 13:41:54 +0530

    mongodb setup

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,30 @@
-import express from 'express'
+require('dotenv-flow').config()
+import express, { Express } from 'express'
 
 // middlewares
 import cors from 'cors'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
-const app = express()
+const PORT:number = parseInt(process.env.PORT || '3000', 10)
+const DB:string = process.env.DB
+
+const app:Express = express()
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const { connection } = mongoose;
 
 // middlewares
 app.use(cors())
+app.use(bodyParser.json({
+  type: 'application/json'
+}))
 
-app.listen(3000, () => {
-  console.log('Listening on 3000')
+connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
 })

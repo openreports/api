b8fdb13f73d5a4854107b293473f41eca170d245
Dhruv Jain <dhruvjainpenny@gmail.com>
2019-10-08 12:08:57 +0530

    add / route and npm scripts

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,8 @@
 import UserController from '../controllers/user'
 
 export default (app) => {
+  app.get('/', (_req, res) => {
+    res.send('Hello from the other side of Open Reports')
+  })
   app.post('/users', UserController.create)
 }
